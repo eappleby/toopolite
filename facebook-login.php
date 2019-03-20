c69930c982a38ce28eefd262fa716b41adfdf2dd
Evan Appleby <evan@pubexchange.com>
2019-03-20 10:44:01 -0400

    test

--- facebook-login.php
+++
@@ -1,7 +0,0 @@
-		<div id="login" style="height: 20px;">
-			<div id="loginText" style="float: left; margin-right: 3px;"></div>
-			<div id="loginLinkWrapper" style="float: left; margin-right: 3px;"><a id="loginLink"></a></div>
-			<div id="loginTextAfterLink" style="float: left;"></div>
-			<div id="anonymousLink" style="float: left; margin-left: 20px;"></div>
-			<div id="loginInfo"></div>
-		</div>
