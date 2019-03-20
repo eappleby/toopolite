2a92505f949c0f10d8aee316c4d0a1c6dc5f7aac
Evan Appleby <evan@pubexchange.com>
2019-03-20 13:11:10 -0400

    fix FB

--- js/outside-js.php
+++ js/outside-js.php
@@ -70,7 +70,27 @@
 				document.getElementById('fbForceLogin').style.visibility = "visible";
 <?php endif ?>
 				fbConnect.onclick = function() {
-					FB.login(function(response) {}, {scope:'email'});
+					// https://developers.facebook.com/docs/reference/javascript/FB.getLoginStatus/
+					FB.getLoginStatus(function(response) {
+						if (response.status === 'connected') {
+							// The user is logged in and has authenticated your
+							// app, and response.authResponse supplies
+							// the user's ID, a valid access token, a signed
+							// request, and the time the access token 
+							// and signed request each expire.
+							var uid = response.authResponse.userID;
+							var accessToken = response.authResponse.accessToken;
+						} else if (response.status === 'not_authorized') {
+							// The user hasn't authorized your application.  They
+							// must click the Login button, or you must call FB.login
+							// in response to a user gesture, to launch a login dialog.
+						} else {
+							// The user isn't logged in to Facebook. You can launch a
+							// login dialog with a user gesture, but the user may have
+							// to log in to Facebook before authorizing your application.
+							FB.login(function(response) {}, {scope:'email'});
+						}
+					});
 				};
 			}
 		}
